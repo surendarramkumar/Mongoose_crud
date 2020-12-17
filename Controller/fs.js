2317ea89945098096481eaf849137ce12c5c98bf
surendarramkumar <ramkumar56743@gmail.com>
2020-12-17 12:33:18 +0530

    readFs Stream

--- Controller/fs.js
+++ Controller/fs.js
@@ -1,17 +1,21 @@
 const fs=require('fs')
-
-
+var data=''
 module.exports.fs_examp=(req,res)=>{
+let readStream=fs.createReadStream('./public/test.txt')
 
-fs.readFile('test.txt',(err,fd)=>{
-if(err){
-console.log(err)
-}
-else{
-    res.send(fd)
-    console.log("success")
-}
+readStream.on('data',(chunk)=>{
+data+=chunk
 })
-
-
+readStream.on('end',()=>{
+    res.send({
+        Message:"success",
+        Data:data
+    })     
+})
+readStream.on('error',(err)=>{
+    console.log(err.stack)
+    res.send({
+        Message:"some Error",
+    })
+})
 }
